af3f8175d2ddba2c4d8f2410711f131c3f0b6d3d
mybuenoday <yoonjibiz@gmail.com>
2024-01-06 21:22:21 +0900

    feat: implement web brwoser features using webview_flutter package.
    Add internet permission to AndroidManifest.xml

---
+++ lib/main_screen.dart
@@ -0,0 +1,31 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) WebView.platform = AndroidWebView();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('MY WEB BROWSER'),
+      ),
+      body: const WebView(
+        initialUrl: 'https://flutter.dev',
+      ),
+    );
+  }
+}
